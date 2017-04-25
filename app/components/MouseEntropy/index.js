e012a90cd9de6368fb44265def99873ef61cf926
KDepp <kdepp.cd@gmail.com>
2017-04-25 23:47:47 +0800

    add Component: MouseEntropy

---
+++ app/components/MouseEntropy/index.js
@@ -0,0 +1,135 @@
+/* eslint no-multi-spaces: "off", key-spacing: "off", jsx-a11y/no-static-element-interactions: "off" */
+
+import React from 'react';
+
+import { byEightDirection, throttleSync } from './sampling';
+import Bits from './bits';
+
+
+const ST = {
+  INITIAL:      0x0001,
+  ACTIVE:       0x0002,
+  DONE:         0x0003,
+};
+
+class MouseEntropy extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bitCount: 0,
+      bitData: new Bits([], 0, props.totalBits),
+      status: ST.INITIAL,
+    };
+
+    this.collectMouseEntropy = throttleSync(byEightDirection, 50);
+    this.onMouseMove  = this.onMouseMove.bind(this);
+    this.onTouchMove  = this.onTouchMove.bind(this);
+    this.reset        = this.reset.bind(this);
+  }
+
+  componentDidMount() {
+    this.setEntropyCollector(this.props);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.sampleRate !== this.props.sampleRate) {
+      this.setEntropyCollector(newProps);
+    }
+  }
+
+  onTouchMove(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    this.onMove({
+      x: e.touches[0].clientX,
+      y: e.touches[0].clientY,
+    });
+  }
+
+  onMouseMove(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    this.onMove({
+      x: e.clientX,
+      y: e.clientY,
+    });
+  }
+
+  onMove(pos) {
+    if (this.state.status === ST.DONE) {
+      return;
+    }
+
+    const oldBits = this.state.bitData;
+    const bits    = this.collectMouseEntropy(pos);
+
+    // Note: bits will be null if entropy factory take this move as useless
+    if (!bits)  return;
+
+    oldBits.appendBits(bits);
+
+    if (oldBits.bitCount >= this.props.totalBits) {
+      this.props.onFinish({
+        raw: oldBits.bytes.slice(),
+        text: oldBits.stringify(),
+      });
+    }
+
+    this.setState({
+      bitCount: oldBits.bitCount,
+      status: oldBits.bitCount >= this.props.totalBits ? ST.DONE : ST.ACTIVE,
+    });
+  }
+
+  setEntropyCollector(props = {}) {
+    const { sampleRate } = props;
+
+    if (sampleRate) {
+      this.collectMouseEntropy = throttleSync(byEightDirection, sampleRate);
+    }
+  }
+
+  reset() {
+    this.props.onFinish(null);
+    this.setState({
+      status: ST.INITIAL,
+      bitCount: 0,
+      bitData: new Bits([], 0),
+    });
+  }
+
+  render() {
+    const {
+      totalBits,
+      size,
+    } = this.props;
+    const { bitCount }  = this.state;
+    const percent       = ((100 * bitCount) / totalBits).toFixed(2);
+
+    return (
+      <div style={{ width: `${size}px` }}>
+        <div
+          role="button"
+          onMouseMove={this.onMouseMove}
+          onTouchMove={this.onTouchMove}
+          onClick={this.reset}
+          style={{ textAlign: 'center', background: '#ddd', width: `${size}px`, height: `${size}px`, lineHeight: `${size}px` }}
+        >
+          Click to reset
+        </div>
+        <div style={{ width: `${percent}%`, height: '3px', background: '#dd2066' }}></div>
+      </div>
+    );
+  }
+}
+
+MouseEntropy.propTypes = {
+  onFinish:   React.PropTypes.func.isRequired,
+  totalBits:  React.PropTypes.number.isRequired,
+  size:       React.PropTypes.number.isRequired,
+  sampleRate: React.PropTypes.number.isRequired,
+};
+
+export default MouseEntropy;
