7f288eea6caf7aad4541bcf53987a574cfb0dbf1
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-05-29 22:27:46 +0700

    Test ButtonJoinSeat and add rotating icon if they seat is pending.

--- app/components/Seat/tests/ButtonJoinSeat.test.js
+++ app/components/Seat/tests/ButtonJoinSeat.test.js
@@ -8,34 +8,29 @@ import ButtonJoinSeat from '../ButtonJoinSeat';
 describe('components.seat.ButtonJoinSeat', () => {
   describe('default state', () => {
     const props = {
-      coords: [1, 1],
-      onClickHandler: () => {},
+      coords: [0, 0],
       pending: false,
+      onClickHandler: () => {},
     };
     it('should show "Join" message', () => {
       const el = mount(<ButtonJoinSeat {...props} />);
 
       expect(el.find('.fa-plus').length).toEqual(1);
-
-      // const la2 = el.findWhere((n) => n.text() === 'Join');
-      // expect(la2.length).toEqual(1);
+      expect(el.text()).toEqual('Join');
     });
   });
 
-  // describe('after join click', () => {
-  //   const props = {
-  //     coords: [],
-  //     onClickHandler: () => {},
-  //     pending: true,
-  //   };
-  //   it('should render joining animation', () => {
-  //     const el = shallow(<ButtonJoinSeat {...props} />);
-  //
-  //     const la = el.findWhere((n) => n.find('.fa-refresh'));
-  //     expect(la.length).toEqual(1);
-  //
-  //     const la2 = el.findWhere((n) => n.text() === 'Join');
-  //     expect(la2.length).toEqual(1);
-  //   });
-  // });
+  describe('after join click', () => {
+    const props = {
+      coords: [0, 0],
+      pending: true,
+      onClickHandler: () => {},
+    };
+    it('should render joining animation', () => {
+      const el = mount(<ButtonJoinSeat {...props} />);
+
+      expect(el.find('.fa-refresh').length).toEqual(1);
+      expect(el.text()).toEqual('Pending');
+    });
+  });
 });
