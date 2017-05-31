96b713300a8179de46edf8d1fdc94cf01be7a572
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-05-31 17:01:45 +0700

    Refactor ActionBar and Table to separate styles and breakdown components

---
+++ app/components/Table/TableMenu.js
@@ -0,0 +1,45 @@
+/** TableMenu.js
+ * Created by zobroj on 20170531
+ */
+
+import React from 'react';
+
+import { ActionButton } from '../ActionBar';
+import { Wrapper } from './styles';
+
+const TableMenu = ({
+  onLeave,
+  onSitout,
+  pending,
+  sitout,
+}) => {
+  // Note: sitout value possibilities
+  // sitout > 0, for enabled "play"
+  // sitout === 0, for disabled "play"
+  // sitout === undefined, for enabled "pause"
+  // sitout === null, for disabled "pause"
+  const icon = (typeof sitout === 'number') ? 'fa fa-play' : 'fa fa-pause';
+  const isToggleSitoutDisabled = sitout === 0 || sitout === null;
+  return (
+    <Wrapper className="anon-wrapper">
+      <ActionButton className="action-button-1" size="small" onClick={onLeave} icon="fa fa-sign-out" />
+      { !pending &&
+        <ActionButton
+          className="action-button-2"
+          size="small"
+          icon={icon}
+          onClick={onSitout}
+          disabled={isToggleSitoutDisabled}
+        />
+      }
+    </Wrapper>
+  );
+};
+TableMenu.propTypes = {
+  pending: React.PropTypes.bool,
+  onLeave: React.PropTypes.any,
+  onSitout: React.PropTypes.any,
+  sitout: React.PropTypes.any,
+};
+
+export default TableMenu;
