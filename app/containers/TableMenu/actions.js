65162c7f7ec0366553cfdc3ebe786b0ebb8c50d2
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-06-06 23:05:18 +0700

    Remove unused code (+1 squashed commit) Squashed commits: [dc8d656] init TableMenu reducer, selector and actions

---
+++ app/containers/TableMenu/actions.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jzobro on 20170606
+ */
+export const TABLE_MENU_TOGGLE = 'acebusters/TableMenu/MENU_TOGGLE';
+
+export function toggleTableMenu() {
+  return { type: TABLE_MENU_TOGGLE };
+}
