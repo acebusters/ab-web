fa822991ee24b560c4b52865cdc21b24d9f93bf8
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-06-09 18:46:47 +0700

    test of storybook with ActionBar component, uncomment the `this.table…` file in the ActionBar constructor to break storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,8 @@
-/* eslint no-ignore: "off", global-require: "off" */
 import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../app/components/TableMenu/stories');
-  // require('../app/components/ActionBar/stories');
+  require('../app/components/ActionBar/stories');
 }
 
 configure(loadStories, module);
