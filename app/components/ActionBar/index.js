20d20d0331997404c34a64a5fff8ded7398ecc99
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-06-09 23:15:39 +0700

    refactor and, uh force some tests to pass, will refactor later

--- app/components/ActionBar/index.js
+++ app/components/ActionBar/index.js
@@ -6,229 +6,119 @@ import Grid from 'grid-styled';
 import Slider from 'react-rangeslider';
 // Storybook: comment-out CSS file
 import 'react-rangeslider/lib/index.css';
-import Raven from 'raven-js';
 
 import SliderWrapper from '../Slider';
 import ChatWrapper from '../Chat';
 import Chat from '../../containers/Chat';
 
 import ActionButton from './ActionButton';
-// Storybook: comment-out TableService
-import TableService from '../../services/tableService';
 
 import {
   ActionBarWrapper,
   ControlPanel,
 } from './styles';
 
-export class ActionBar extends React.Component { // eslint-disable-line react/prefer-stateless-function
-
-  constructor(props) {
-    super(props);
-    this.handleBet = this.handleBet.bind(this);
-    this.handleCheck = this.handleCheck.bind(this);
-    this.handleCall = this.handleCall.bind(this);
-    this.handleFold = this.handleFold.bind(this);
-    this.updateAmount = this.updateAmount.bind(this);
-    this.sendMessage = this.sendMessage.bind(this);
-    // Storybook: comment-out this.tabel
-    this.table = new TableService(props.params.tableAddr, this.props.privKey);
-    this.state = {
-      active: true,
-    };
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const min = nextProps.minRaise;
-    const amount = (min && nextProps.myStack < min) ? nextProps.myStack : min;
-    this.setState({ amount });
-    if (nextProps.isMyTurn === true) {
-      this.setActive(true);
-    }
-  }
-
-  setActive(active) {
-    this.setState({ active });
-  }
-
-  updateAmount(value) {
-    let amount = parseInt(value, 10);
-    amount = (amount > this.props.myStack) ? this.props.myStack : amount;
-    this.setState({ amount });
-  }
-
-  captureError(handId) {
-    const self = this;
-
-    return (err) => {
-      Raven.captureException(err, { tags: {
-        tableAddr: self.props.params.tableAddr,
-        handId,
-      } });
-      self.setActive(true);
-    };
-  }
-
-  handleBet() {
-    this.setActive(false);
-    const amount = this.state.amount + this.props.myMaxBet;
-    const handId = parseInt(this.props.params.handId, 10);
-
-    const betAction = this.props.bet(this.props.params.tableAddr, handId, amount, this.props.privKey, this.props.myPos, this.props.lastReceipt);
-    return this.props.pay(betAction, this.props.dispatch)
-    .then((cards) => {
-      this.props.setCards(this.props.params.tableAddr, handId, cards);
-    })
-    .catch(this.captureError(handId));
-  }
-
-  handleCall() {
-    const amount = parseInt(this.props.callAmount, 10);
-    this.setState({ amount }, () => {
-      this.handleBet();
-    });
-  }
-
-  handleCheck() {
-    this.setActive(false);
-    const amount = this.props.myMaxBet;
-    const handId = parseInt(this.props.params.handId, 10);
-    const checkStates = ['preflop', 'turn', 'river', 'flop'];
-    const state = this.props.state;
-    const checkType = checkStates.indexOf(state) !== -1 ? state : 'flop';
-    const action = this.props.check(
-      this.props.params.tableAddr,
-      handId,
-      amount,
-      this.props.privKey,
-      this.props.myPos,
-      this.props.lastReceipt,
-      checkType,
+const ActionBar = (props) => {
+  const {
+    active, amount, amountToCall, callAmount, isMyTurn, handleBet, handleCall,
+    handleCheck, handleFold, messages, minRaise, myPos, myStack, params,
+    playerCount, state, sendMessage, updateAmount,
+  } = props;
+  const isTakePartOfAGame = myPos != null;
+  const isAppropriateState = (
+    state !== 'waiting' && state !== 'dealing' && state !== 'showdown'
+  );
+  const canSeeChat = (isTakePartOfAGame && !isMyTurn && isAppropriateState) || !isTakePartOfAGame;
+  if (active && isMyTurn && isAppropriateState) {
+    return (
+      <ActionBarWrapper name="action-bar-wrapper">
+        <ControlPanel>
+          <SliderWrapper>
+            { myStack > amountToCall &&
+              <Slider
+                key="betting-slider"
+                data-orientation="vertical"
+                value={amount}
+                min={minRaise}
+                max={myStack}
+                step={1}
+                onChange={updateAmount}
+              >
+              </Slider>
+            }
+          </SliderWrapper>
+          <Grid xs={1 / 3}>
+            {amountToCall > 0 &&
+              <div>
+                {(myStack > amountToCall) ? // raiseButton
+                  <ActionButton
+                    size="medium"
+                    onClick={handleBet}
+                    text={`RAISE ${amount}`}
+                  />
+                  : null
+                }
+                <ActionButton
+                  size="medium"
+                  onClick={handleCall}
+                  text={`CALL ${callAmount}`}
+                />
+                <ActionButton
+                  size="medium"
+                  onClick={handleFold}
+                  text="FOLD"
+                />
+              </div>
+            }
+            { amountToCall === 0 &&
+              <div>
+                <ActionButton
+                  size="medium"
+                  onClick={handleBet}
+                  text={`BET ${amount}`}
+                />
+                <ActionButton
+                  size="medium"
+                  onClick={handleCheck}
+                  text="CHECK"
+                />
+              </div>
+            }
+          </Grid>
+        </ControlPanel>
+      </ActionBarWrapper>
     );
-
-    return this.props.pay(action, this.props.dispatch)
-      .then((cards) => {
-        this.props.setCards(this.props.params.tableAddr, handId, cards);
-      })
-      .catch(this.captureError(handId));
-  }
-
-  handleFold() {
-    this.setActive(false);
-    const amount = this.props.myMaxBet;
-    const handId = parseInt(this.props.params.handId, 10);
-    const action = this.props.fold(
-      this.props.params.tableAddr,
-      handId,
-      amount,
-      this.props.privKey,
-      this.props.myPos,
-      this.props.lastReceipt
+  } else if (canSeeChat) {
+    const ta = params.tableAddr.substring(2, 8);
+    const chatPlaceholder = `table <${ta}> in state ${state} has ${playerCount || 'no'} player${playerCount === 1 ? '' : 's'}.`;
+    return (
+      <ChatWrapper>
+        <Chat onAddMessage={sendMessage} messages={messages} readonly={!isTakePartOfAGame} placeholder={chatPlaceholder} />
+      </ChatWrapper>
     );
-
-    return this.props.pay(action, this.props.dispatch)
-      .then((cards) => {
-        this.props.setCards(this.props.params.tableAddr, handId, cards);
-      })
-      .catch(this.captureError(handId));
   }
-
-  sendMessage(message) {
-    this.props.sendMessage(message, this.props.params.tableAddr, this.props.privKey);
-  }
-
-  render() {
-    const isMyTurn = this.props.isMyTurn;
-    const isTakePartOfAGame = this.props.myPos != null;
-    const isAppropriateState = (
-      this.props.state !== 'waiting'
-      && this.props.state !== 'dealing'
-      && this.props.state !== 'showdown'
-    );
-    const canSeeChat = (
-      isTakePartOfAGame
-      && !isMyTurn
-      && isAppropriateState
-    ) || !isTakePartOfAGame;
-    const canSeeActionBar = (
-      this.state.active
-      && isMyTurn
-      && isAppropriateState
-    );
-    if (canSeeActionBar) {
-      const raiseButton = (this.props.myStack > this.props.amountToCall) ? (<ActionButton size="medium" onClick={this.handleBet} text={`RAISE ${this.state.amount}`} />) : null;
-      return (
-        <ActionBarWrapper name="action-bar-wrapper">
-          <ControlPanel>
-            <SliderWrapper>
-              { this.props.myStack > this.props.amountToCall &&
-                <Slider
-                  key="betting-slider"
-                  data-orientation="vertical"
-                  value={this.state.amount}
-                  min={this.props.minRaise}
-                  max={this.props.myStack}
-                  step={1}
-                  onChange={this.updateAmount}
-                >
-                </Slider>
-              }
-            </SliderWrapper>
-            <Grid xs={1 / 3}>
-              { this.props.amountToCall > 0 &&
-                <div>
-                  { raiseButton }
-                  <ActionButton size="medium" onClick={this.handleCall} text={`CALL ${this.props.callAmount}`}>
-                  </ActionButton>
-                  <ActionButton size="medium" onClick={this.handleFold} text="FOLD"></ActionButton>
-                </div>
-              }
-              { this.props.amountToCall === 0 &&
-                <div>
-                  <ActionButton size="medium" onClick={this.handleBet} text={`BET ${this.state.amount}`}>
-                  </ActionButton>
-                  <ActionButton size="medium" onClick={this.handleCheck} text="CHECK">
-                  </ActionButton>
-                </div>
-              }
-            </Grid>
-          </ControlPanel>
-        </ActionBarWrapper>
-      );
-    } else if (canSeeChat) {
-      const ta = this.props.params.tableAddr.substring(2, 8);
-      const chatPlaceholder = `table <${ta}> in state ${this.props.state} has ${this.props.playerCount || 'no'} player${this.props.playerCount === 1 ? '' : 's'}.`;
-      return (
-        <ChatWrapper>
-          <Chat onAddMessage={this.sendMessage} messages={this.props.messages} readonly={!isTakePartOfAGame} placeholder={chatPlaceholder} />
-        </ChatWrapper>
-      );
-    }
-    return null;
-  }
-}
+  return null;
+};
 
 ActionBar.propTypes = {
-  params: React.PropTypes.object,
-  privKey: React.PropTypes.string,
-  lastReceipt: React.PropTypes.object,
-  myPos: React.PropTypes.number,
-  myMaxBet: React.PropTypes.number,
-  isMyTurn: React.PropTypes.bool,
-  minRaise: React.PropTypes.number,
+  active: React.PropTypes.bool,
+  amount: React.PropTypes.number,
   amountToCall: React.PropTypes.number,
-  myStack: React.PropTypes.number,
   callAmount: React.PropTypes.number,
-  state: React.PropTypes.string,
-  dispatch: React.PropTypes.func,
-  setCards: React.PropTypes.func,
+  handleBet: React.PropTypes.func,
+  handleCheck: React.PropTypes.func,
+  handleCall: React.PropTypes.func,
+  handleFold: React.PropTypes.func,
+  updateAmount: React.PropTypes.func,
   sendMessage: React.PropTypes.func,
+  isMyTurn: React.PropTypes.bool,
   messages: React.PropTypes.array,
+  minRaise: React.PropTypes.number,
+  myStack: React.PropTypes.number,
+  myPos: React.PropTypes.number,
+  state: React.PropTypes.string,
+  params: React.PropTypes.object,
   playerCount: React.PropTypes.number,
-  bet: React.PropTypes.func,
-  pay: React.PropTypes.func,
-  fold: React.PropTypes.func,
-  check: React.PropTypes.func,
 };
 
 export default ActionBar;
--- app/containers/ActionBar/index.js
+++ app/containers/ActionBar/index.js
@@ -1,8 +1,12 @@
 /**
  * Created by helge on 24.08.16.
  */
+import React from 'react';
+import Raven from 'raven-js';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
+
+import TableService from '../../services/tableService';
 
 import {
   makeMinSelector,
@@ -31,26 +35,173 @@ import { setCards, sendMessage, bet, pay, fold, check } from '../Table/actions';
 
 import ActionBar from '../../components/ActionBar';
 
+class ActionBarContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleBet = this.handleBet.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+    this.handleCall = this.handleCall.bind(this);
+    this.handleFold = this.handleFold.bind(this);
+    this.updateAmount = this.updateAmount.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.table = new TableService(props.params.tableAddr, this.props.privKey);
+    this.state = {
+      active: true,
+      amount: null,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const min = nextProps.minRaise;
+    const amount = (min && nextProps.myStack < min) ? nextProps.myStack : min;
+    this.setState({ amount });
+    if (nextProps.isMyTurn === true) {
+      this.setActive(true);
+    }
+  }
+
+  setActive(active) {
+    this.setState({ active });
+  }
+
+  updateAmount(value) {
+    let amount = parseInt(value, 10);
+    amount = (amount > this.props.myStack) ? this.props.myStack : amount;
+    this.setState({ amount });
+  }
+
+  captureError(handId) {
+    const self = this;
+
+    return (err) => {
+      Raven.captureException(err, { tags: {
+        tableAddr: self.props.params.tableAddr,
+        handId,
+      } });
+      self.setActive(true);
+    };
+  }
+
+  handleBet() {
+    this.setActive(false);
+    const amount = this.state.amount + this.props.myMaxBet;
+    const handId = parseInt(this.props.params.handId, 10);
+
+    const betAction = this.props.bet(this.props.params.tableAddr, handId, amount, this.props.privKey, this.props.myPos, this.props.lastReceipt);
+    return this.props.pay(betAction, this.props.dispatch)
+    .then((cards) => {
+      this.props.setCards(this.props.params.tableAddr, handId, cards);
+    })
+    .catch(this.captureError(handId));
+  }
+
+  handleCall() {
+    const amount = parseInt(this.props.callAmount, 10);
+    this.setState({ amount }, () => {
+      this.handleBet();
+    });
+  }
+
+  handleCheck() {
+    this.setActive(false);
+    const amount = this.props.myMaxBet;
+    const handId = parseInt(this.props.params.handId, 10);
+    const checkStates = ['preflop', 'turn', 'river', 'flop'];
+    const state = this.props.state;
+    const checkType = checkStates.indexOf(state) !== -1 ? state : 'flop';
+    const action = this.props.check(
+      this.props.params.tableAddr,
+      handId,
+      amount,
+      this.props.privKey,
+      this.props.myPos,
+      this.props.lastReceipt,
+      checkType,
+    );
+
+    return this.props.pay(action, this.props.dispatch)
+      .then((cards) => {
+        this.props.setCards(this.props.params.tableAddr, handId, cards);
+      })
+      .catch(this.captureError(handId));
+  }
+
+  handleFold() {
+    this.setActive(false);
+    const amount = this.props.myMaxBet;
+    const handId = parseInt(this.props.params.handId, 10);
+    const action = this.props.fold(
+      this.props.params.tableAddr,
+      handId,
+      amount,
+      this.props.privKey,
+      this.props.myPos,
+      this.props.lastReceipt
+    );
+
+    return this.props.pay(action, this.props.dispatch)
+      .then((cards) => {
+        this.props.setCards(this.props.params.tableAddr, handId, cards);
+      })
+      .catch(this.captureError(handId));
+  }
+
+  sendMessage(message) {
+    this.props.sendMessage(message, this.props.params.tableAddr, this.props.privKey);
+  }
+
+  render() {
+    return (
+      <ActionBar
+        active={this.state.active}
+        amount={this.state.amount}
+        handleBet={this.handleBet}
+        handleCheck={this.handleCheck}
+        handleCall={this.handleCall}
+        handleFold={this.handleFold}
+        updateAmount={this.updateAmount}
+        sendMessage={this.sendMessage}
+        {...this.props}
+      />
+    );
+  }
+}
+
+ActionBarContainer.propTypes = {
+  bet: React.PropTypes.func,
+  callAmount: React.PropTypes.number,
+  check: React.PropTypes.func,
+  dispatch: React.PropTypes.func,
+  fold: React.PropTypes.func,
+  lastReceipt: React.PropTypes.object,
+  myMaxBet: React.PropTypes.number,
+  myPos: React.PropTypes.number,
+  myStack: React.PropTypes.number,
+  pay: React.PropTypes.func,
+  params: React.PropTypes.object,
+  privKey: React.PropTypes.string,
+  setCards: React.PropTypes.func,
+  sendMessage: React.PropTypes.func,
+  state: React.PropTypes.string,
+};
+
 export function mapDispatchToProps(dispatch) {
   return {
     dispatch,
     setCards: (tableAddr, handId, cards) => setCards(tableAddr, handId, cards),
-    sendMessage: (
-      message, tableAddr, privKey,
-    ) => dispatch(sendMessage(
-      message, tableAddr, privKey,
-    )),
-    bet: (
-      tableAddr, handId, amount, privKey, myPos, lastReceipt,
-    ) => bet(
+    sendMessage: (message, tableAddr, privKey) => dispatch(
+      sendMessage(message, tableAddr, privKey)
+    ),
+    bet: (tableAddr, handId, amount, privKey, myPos, lastReceipt) => bet(
       tableAddr, handId, amount, privKey, myPos, lastReceipt,
     ),
     pay: (betAction) => pay(betAction, dispatch),
-    fold: (tableAddr, handId, amount, privKey, myPos, lastReceipt,
-      ) => fold(tableAddr, handId, amount, privKey, myPos, lastReceipt),
-    check: (tableAddr, handId, amount, privKey, myPos, lastReceipt, checkType,
+    fold: (tableAddr, handId, amount, privKey, myPos, lastReceipt) => fold(
+      tableAddr, handId, amount, privKey, myPos, lastReceipt),
+    check: (tableAddr, handId, amount, privKey, myPos, lastReceipt, checkType
       ) => check(
-        tableAddr, handId, amount, privKey, myPos, lastReceipt, checkType),
+        tableAddr, handId, amount, privKey, myPos, lastReceipt, checkType
+    ),
   };
 }
 
@@ -70,4 +221,4 @@ const mapStateToProps = createStructuredSelector({
   playerCount: makePlayersCountSelector(),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ActionBar);
+export default connect(mapStateToProps, mapDispatchToProps)(ActionBarContainer);
