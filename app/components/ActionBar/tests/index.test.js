6b1e715dff68c8bcc7d1c5a882f486c42405b871
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-06-10 14:18:36 +0700

    share tests between storybook and jest test runner

--- app/components/ActionBar/tests/index.test.js
+++ app/components/ActionBar/tests/index.test.js
@@ -7,182 +7,124 @@ import { shallow } from 'enzyme';
 import ActionBar from '../index';
 import ActionButton from '../ActionButton';
 
-import {
-  test1, test2, test3, test4, test5, test6, test7,
-} from './tests';
+import tests from './tests';
 
-describe(test1.describe, () => {
-  it(test1.it, () => {
+describe(tests[0].describe, () => {
+  it(tests[0].it, () => {
     const actionBar = shallow(
-      <ActionBar {...test1.props} />
+      <ActionBar {...tests[0].props} />
     );
     expect(actionBar.find('ActionBarComponent').length).toBe(0);
   });
 });
 
-describe(test2.describe, () => {
-  it(test2.it, () => {
+describe(tests[1].describe, () => {
+  it(tests[1].it, () => {
     const actionBar = shallow(
-      <ActionBar {...test2.props} />
+      <ActionBar {...tests[1].props} />
     );
     expect(actionBar.find('ActionBarComponent').length).toBe(0);
   });
 });
 
-describe(test3.describe, () => {
-  it(test3.it, () => {
+describe(tests[2].describe, () => {
+  it(tests[2].it, () => {
     const actionBar = shallow(
-      <ActionBar {...test3.props} />
+      <ActionBar {...tests[2].props} />
     );
     expect(actionBar.find({ name: 'action-bar-wrapper' }).length).toEqual(1);
   });
 });
 
-describe(test4.describe, () => {
-  it(test4.it, () => {
+describe(tests[3].describe, () => {
+  it(tests[3].it, () => {
     const actionBar = shallow(
-      <ActionBar {...test4.props} />
+      <ActionBar {...tests[3].props} />
     );
     expect(actionBar.find('ActionBarComponent').length).toBe(0);
   });
 });
 
-describe(test5.describe, () => {
-  it(test5.it, () => {
+describe(tests[4].describe, () => {
+  it(tests[4].it, () => {
     const actionBar = shallow(
-      <ActionBar {...test5.props} />
+      <ActionBar {...tests[4].props} />
     );
     expect(actionBar.find(ActionButton).last().props().text).toBe('FOLD');
   });
 });
 
-describe(test6.describe, () => {
-  it(test6.it, () => {
+describe(tests[5].describe, () => {
+  it(tests[5].it, () => {
     const actionBar = shallow(
-      <ActionBar {...test6.props} />
+      <ActionBar {...tests[5].props} />
     );
     expect(actionBar.find(ActionButton).length).toBe(2);
   });
 });
 
-describe(test7.describe, () => {
-  it(test7.it, () => {
+describe(tests[6].describe, () => {
+  it(tests[6].it, () => {
     const actionBar = shallow(
-      <ActionBar {...test7.props} />
+      <ActionBar {...tests[6].props} />
     );
     // actionBar.instance().componentWillReceiveProps(props);
     expect(actionBar.find(ActionButton).first().props().text).toEqual('BET 2000');
   });
 });
 
-describe('ActionBar', () => {
-  it('should render the RAISE Button with correct min amount', () => {
-    const props = {
-      active: true,
-      amount: 5000,
-      state: 'flop',
-      params: {
-        tableAddr: '0x123',
-      },
-      isMyTurn: true,
-      minRaise: 5000,
-      amountToCall: 1000,
-      myStack: 10000,
-    };
+describe(tests[7].describe, () => {
+  it(tests[7].it, () => {
     const actionBar = shallow(
-      <ActionBar {...props} />
+      <ActionBar {...tests[7].props} />
     );
     // actionBar.instance().componentWillReceiveProps(props);
     expect(actionBar.find(ActionButton).first().props().text).toEqual('RAISE 5000');
   });
+});
 
-  it('should set Bet to all in amount', () => {
-    const props = {
-      active: true,
-      amount: 1750,
-      state: 'flop',
-      params: {
-        tableAddr: '0x123',
-      },
-      isMyTurn: true,
-      myStack: 1750,
-      amountToCall: 0,
-    };
+describe(tests[8].describe, () => {
+  it(tests[8].it, () => {
     const actionBar = shallow(
-      <ActionBar {...props} />
+      <ActionBar {...tests[8].props} />
     );
     // actionBar.instance().updateamount(2000);
     expect(actionBar.find(ActionButton).first().props().text).toEqual('BET 1750');
   });
+});
 
-  it('should render the Check Button when amount to call is 0', () => {
-    const props = {
-      active: true,
-      state: 'flop',
-      params: {
-        tableAddr: '0x123',
-      },
-      isMyTurn: true,
-      amountToCall: 0,
-    };
+describe(tests[9].describe, () => {
+  it(tests[9].it, () => {
     const actionBar = shallow(
-      <ActionBar {...props} />
+      <ActionBar {...tests[9].props} />
     );
     expect(actionBar.find(ActionButton).nodes[1].props.text).toEqual('CHECK');
   });
+});
 
-  it('should render the Call Button when amount to call is greater 0', () => {
-    const props = {
-      active: true,
-      amount: 1000,
-      state: 'preflop',
-      params: {
-        tableAddr: '0x123',
-      },
-      isMyTurn: true,
-      callAmount: 1000,
-      amountToCall: 1000,
-      myStack: 2000,
-    };
+describe(tests[10].describe, () => {
+  it(tests[10].it, () => {
     const actionBar = shallow(
-      <ActionBar {...props} />
+      <ActionBar {...tests[10].props} />
     );
     expect(actionBar.find(ActionButton).nodes[1].props.text).toEqual('CALL 1000');
   });
+});
 
-  it('should not render the Raise Button if amount to call is bigger than my stack', () => {
-    const props = {
-      active: true,
-      amount: 800,
-      state: 'preflop',
-      params: {
-        tableAddr: '0x123',
-      },
-      isMyTurn: true,
-      callAmount: 800,
-      amountToCall: 1000,
-      myStack: 800,
-    };
+describe(tests[11].describe, () => {
+  it(tests[11].it, () => {
     const actionBar = shallow(
-      <ActionBar {...props} />
+      <ActionBar {...tests[11].props} />
     );
     expect(actionBar.find(ActionButton).nodes[0].props.text).toEqual('CALL 800');
     expect(actionBar.find(ActionButton).nodes.length).toEqual(2);
   });
+});
 
-  it('should disappear after action was taken', () => {
-    const props = {
-      active: false,
-      state: 'flop',
-      params: {
-        tableAddr: '0x123',
-        handId: 1,
-      },
-      isMyTurn: true,
-      amountToCall: 1000,
-    };
+describe(tests[12].describe, () => {
+  it(tests[12].it, () => {
     const actionBar = shallow(
-      <ActionBar {...props} />
+      <ActionBar {...tests[12].props} />
     );
     // actionBar.instance().setActive(false);
     expect(actionBar.find('ActionBarComponent').length).toBe(0);
--- app/components/ActionBar/tests/stories/index.js
+++ app/components/ActionBar/tests/stories/index.js
@@ -7,9 +7,7 @@ import {
   // number,
 } from '@storybook/addon-knobs';
 
-import {
-  combine, test1, test2, test3, test4, test5, test6, test7,
-} from '../tests';
+import tests, { combine } from '../tests';
 
 import ActionBar from '../../index';
 
@@ -17,24 +15,9 @@ const stories = storiesOf('ActionBar', module);
 
 stories.addDecorator(withKnobs);
 
-stories.add(combine(test1.describe, test1.it), () => (
-  <ActionBar {...test1.props} />
-));
-stories.add(combine(test2.describe, test2.it), () => (
-  <ActionBar {...test2.props} />
-));
-stories.add(combine(test3.describe, test3.it), () => (
-  <ActionBar {...test3.props} />
-));
-stories.add(combine(test4.describe, test4.it), () => (
-  <ActionBar {...test4.props} />
-));
-stories.add(combine(test5.describe, test5.it), () => (
-  <ActionBar {...test5.props} />
-));
-stories.add(combine(test6.describe, test6.it), () => (
-  <ActionBar {...test6.props} />
-));
-stories.add(combine(test7.describe, test7.it), () => (
-  <ActionBar {...test7.props} />
-));
+// iterate over tests and add a story for each
+tests.forEach((test) => {
+  stories.add(combine(test.describe, test.it), () => (
+    <ActionBar {...test.props} />
+  ));
+});
