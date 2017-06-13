35a4155a41f487c59a91e88838ca0a1495d777b0
Alexander Lunyov <isuntc@gmail.com>
2017-06-13 16:25:04 +0800

    Rework refCode validation

--- app/containers/RegisterPage/index.js
+++ app/containers/RegisterPage/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ReCAPTCHA from 'react-google-recaptcha';
-import { Form, Field, reduxForm, SubmissionError, propTypes } from 'redux-form/immutable';
+import { Form, Field, reduxForm, SubmissionError, propTypes, stopAsyncValidation, startAsyncValidation } from 'redux-form/immutable';
 import { browserHistory } from 'react-router';
 // components
 import Container from '../../components/Container';
@@ -27,32 +27,13 @@ const validate = (values) => {
   if (!values.get('captchaResponse')) {
     errors.captchaResponse = 'Required';
   }
-  return errors;
-};
 
-const asyncValidate = (values, dispatch, props) => {
-  const value = values.get('referral') || '';
-  return account
-    .checkReferral(value)
-    .then((response) => {
-      if (response.defaultRef && value.length === 0) {
-        dispatch(props.change('referral', response.defaultRef));
-      }
-      return {};
-    })
-    .catch((err) => {
-      switch (err) {
-        case 400:
-        case 404:
-          return { referral: 'Invalid referral code' };
-        case 418:
-          return { referral: 'Referral code is no longer available' };
-        case 420:
-          return { referral: 'Sorry, signup limit reached, try to signup later' };
-        default:
-          return {};
-      }
-    });
+  const referral = values.get('referral') || '';
+  if (referral.length > 0 && referral.length !== 8) {
+    errors.referral = 'Referral code must have 8 letters';
+  }
+
+  return errors;
 };
 
 const warn = (values) => {
@@ -85,13 +66,20 @@ export class RegisterPage extends React.Component { // eslint-disable-line react
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReferralChange = this.handleReferralChange.bind(this);
+    this.defaultRef = undefined;
   }
 
   handleSubmit(values) {
     // Note: auto increase progress for 3 seconds;
     this.props.setProgress(-3000);
 
-    return account.register(values.get('email'), values.get('captchaResponse'), window.location.origin, values.get('referral')).catch((err) => {
+    return account.register(
+      values.get('email'),
+      values.get('captchaResponse'),
+      window.location.origin,
+      values.get('referral') || this.defaultRef
+    ).catch((err) => {
       // If store account failed, ...
       const errMsg = 'Registration failed!';
       if (err === 409) {
@@ -106,21 +94,64 @@ export class RegisterPage extends React.Component { // eslint-disable-line react
     });
   }
 
+  handleReferralChange(e, value/* , prevValue*/) {
+    if (value.length === 8) {
+      this.defaultRef = undefined;
+      this.props.dispatch(startAsyncValidation('register'));
+      account
+        .checkReferral(value)
+        .then((response) => {
+          if (response.defaultRef) {
+            this.defaultRef = response.defaultRef;
+          }
+          return null;
+        })
+        .catch((err) => {
+          switch (err) {
+            case 400:
+            case 404:
+              return 'Invalid referral code';
+            case 418:
+              return 'Referral code is no longer available';
+            case 420:
+              return 'Sorry, signup limit reached, try to signup later';
+            default:
+              return null;
+          }
+        })
+        .then((error) => {
+          this.props.dispatch(
+            stopAsyncValidation(
+              'register',
+              error ? { referral: error } : {},
+            )
+          );
+        });
+    }
+  }
+
   render() {
-    const { error, handleSubmit, invalid, submitting } = this.props;
+    const { error, handleSubmit, invalid, submitting, asyncValidating } = this.props;
+    console.log(this.props);
 
     return (
       <Container>
         <div>
-          <H1> Register a new account!</H1>
+          <H1>Register a new account!</H1>
           <Form
             onSubmit={handleSubmit(this.handleSubmit)}
           >
             <Field name="email" type="text" component={renderField} label="e-mail" />
-            <Field name="referral" type="text" component={renderField} label="referral code" />
+            <Field
+              name="referral"
+              type="text"
+              component={renderField}
+              label="referral code"
+              onChange={this.handleReferralChange}
+            />
             <Field name="captchaResponse" component={Captcha} />
             {error && <ErrorMessage error={error} />}
-            <Button type="submit" disabled={submitting || invalid} size="large">
+            <Button type="submit" disabled={submitting || invalid || asyncValidating} size="large">
               { (!submitting) ? 'Register' : 'Please wait ...' }
             </Button>
           </Form>
@@ -150,8 +181,8 @@ export default connect(mapStateToProps, mapDispatchToProps)(
   reduxForm({
     form: 'register',
     validate,
-    asyncValidate,
-    asyncBlurFields: [],
+    // asyncValidate,
+    // asyncBlurFields: [],
     warn,
   })(RegisterPage)
 );
