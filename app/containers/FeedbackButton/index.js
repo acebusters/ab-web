78fc2c0c660e6ee5b05f28df3365a2e2954049ab
grabus <grabovalex@gmail.com>
2017-06-15 17:40:59 +0300

    fix indentation

--- app/containers/FeedbackButton/index.js
+++ app/containers/FeedbackButton/index.js
@@ -4,16 +4,16 @@ import Raven from 'raven-js';
 import styled from 'styled-components';
 
 const Link = styled.a`
- cursor: pointer; 
- padding: 5px 10px;
- background-color: cornflowerblue;
- transform: rotate(90deg);
- display: inline-block;
- position: fixed;
- right: -23px;
- top: 150px;
- color: white;
- border-radius: 0 0 3px 3px;
+  cursor: pointer; 
+  padding: 5px 10px;
+  background-color: cornflowerblue;
+  transform: rotate(90deg);
+  display: inline-block;
+  position: fixed;
+  right: -23px;
+  top: 150px;
+  color: white;
+  border-radius: 0 0 3px 3px;
 `;
 
 export class FeedbackButton extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
