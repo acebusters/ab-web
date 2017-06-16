8e9c513858d85fee7c6c5b6ec7aa40536d0600bc
Alexander Lunyov <isuntc@gmail.com>
2017-06-16 15:31:55 +0800

    PurchaseDialog container, extract AmountField component

--- app/containers/PurchaseDialog/index.js
+++ app/containers/PurchaseDialog/index.js
@@ -1,18 +1,17 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-import { Form, Field, reduxForm } from 'redux-form/immutable';
+import { Form, reduxForm } from 'redux-form/immutable';
 import { FormattedMessage } from 'react-intl';
-import BigNumber from 'bignumber.js';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import Label from '../../components/Label';
+import AmountField from '../../components/AmountField';
 import H2 from '../../components/H2';
 import FormGroup from '../../components/Form/FormGroup';
 import { ErrorMessage } from '../../components/FormMessages';
 
-import { isEthereumAddress } from './isEthereumAddress';
 import messages from './messages';
 
 const validate = (values) => {
@@ -21,11 +20,6 @@ const validate = (values) => {
     errors.amount = 'Required';
   }
 
-  if (!values.get('address')) {
-    errors.address = 'Required';
-  } else if (!isEthereumAddress(values.get('address'))) {
-    errors.address = 'Invalid Ethereum Address.';
-  }
   return errors;
 };
 
@@ -44,44 +38,29 @@ const renderField = ({ input, label, type, meta: { touched, error, warning } }) 
 );
 /* eslint-enable react/prop-types */
 
-class TransferDialog extends React.Component { // eslint-disable-line react/prefer-stateless-function
+class PurchaseDialog extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(values) {
-    this.props.handleTransfer(
-      values.get('address'),
-      values.get('amount')
-    );
+    const amount = Number(values.get('amount', '').replace(',', '.'));
+    this.props.handleTransfer(values.get('address'), amount);
   }
 
   render() {
     const { error, handleSubmit, submitting, amountUnit, maxAmount } = this.props;
 
-    const limitAmount = (value) => {
-      const numValue = Math.max(0, Number(value));
-
-      return maxAmount.gte(new BigNumber(numValue)) ? numValue : maxAmount.toNumber();
-    };
-
     return (
       <div>
         <H2><FormattedMessage {...messages.header} /></H2>
         <Form onSubmit={handleSubmit(this.handleSubmit)}>
-          <Field
+          <AmountField
             name="amount"
             component={renderField}
-            type="number"
-            label={`Amount (${amountUnit})`}
-            normalize={maxAmount && limitAmount}
-          />
-          <Field
-            name="address"
-            component={renderField}
-            type="text"
-            label="Ethereum address"
+            label={`NTZ amount for purchase (${amountUnit})`}
+            maxAmount={maxAmount}
           />
           {error && <strong>{error}</strong>}
           <div>
@@ -93,7 +72,7 @@ class TransferDialog extends React.Component { // eslint-disable-line react/pref
   }
 }
 
-TransferDialog.propTypes = {
+PurchaseDialog.propTypes = {
   submitting: PropTypes.bool,
   maxAmount: PropTypes.object, // BigNumber
   amountUnit: PropTypes.string,
@@ -114,8 +93,8 @@ const mapStateToProps = () => ({
 
 export default connect(mapStateToProps, mapDispatchToProps)(
   reduxForm({
-    form: 'transfer',
+    form: 'purchase',
     validate,
     warn,
-  })(TransferDialog)
+  })(PurchaseDialog)
 );
