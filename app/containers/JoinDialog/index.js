9199ec5d813ab194faf052e50d79e203d6134867
Johann Barbie <johannbarbie@me.com>
2017-06-22 13:57:13 +0200

    fixed tests

--- app/containers/JoinDialog/index.js
+++ app/containers/JoinDialog/index.js
@@ -10,14 +10,13 @@ import { makeSbSelector } from '../Table/selectors';
 import {
   makeSelectProxyAddr,
 } from '../AccountProvider/selectors';
-import { NTZ_DECIMALS } from '../../app.config';
 
 export class JoinDialog extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   constructor(props) {
     super(props);
     this.state = {
-      amount: (props.sb / NTZ_DECIMALS) * 40,
+      amount: props.sb * 40,
     };
     this.updateAmount = this.updateAmount.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -29,16 +28,14 @@ export class JoinDialog extends React.Component { // eslint-disable-line react/p
   }
 
   handleSubmit() {
-    this.props.handleJoin(this.props.pos, this.state.amount * NTZ_DECIMALS);
+    this.props.handleJoin(this.props.pos, this.state.amount);
   }
 
   render() {
-    const ntzSb = this.props.sb / NTZ_DECIMALS;
-    const ntzMin = ntzSb * 40;
-    const ntzTableMax = ntzSb * 200;
-    const ntzBalance = (this.props.balance / NTZ_DECIMALS);
-    const ntzMax = (ntzBalance < ntzTableMax) ? ntzBalance : ntzTableMax;
-    if (ntzBalance < ntzMin) {
+    const min = this.props.sb * 40;
+    const tableMax = this.props.sb * 200;
+    const max = (this.props.balance < tableMax) ? this.props.balance - (this.props.balance % this.props.sb) : tableMax;
+    if (this.props.balance < min) {
       return (
         <div style={{ minWidth: '20em' }}>
           <H2>Sorry!</H2>
@@ -53,14 +50,14 @@ export class JoinDialog extends React.Component { // eslint-disable-line react/p
           data-orientation="vertical"
           value={this.state.amount}
           tooltip={false}
-          min={ntzMin}
-          max={ntzMax}
-          step={ntzSb}
+          min={min}
+          max={max}
+          step={this.props.sb}
           onChange={this.updateAmount}
         >
         </Slider>
-        <div> Max: {ntzMax}</div>
-        <div>{ (this.state) ? this.state.amount : ntzMin }</div>
+        <div> Max: {(max).formatNtz()}</div>
+        <div>{ (this.state) ? (this.state.amount).formatNtz() : (min).formatNtz() }</div>
         <Button onClick={this.handleSubmit}>Join</Button>
       </div>
     );
