a4a6d42543233f3451880d4ff41144ffae2608f5
Johann Barbie <johannbarbie@me.com>
2017-06-22 23:52:57 +0200

    fixed display of 0 amounts

--- app/utils/amountFormater.js
+++ app/utils/amountFormater.js
@@ -2,9 +2,9 @@ import { NTZ_DECIMALS } from '../app.config';
 
 export function formatNtz(babzAmount, decimals = 0, decimalMark = '.', thousandSeparator = ',') {
   const s = babzAmount < 0 ? '-' : '';
-  const amount = Math.abs(Number(babzAmount / NTZ_DECIMALS)) || 0;
-  const i = String(parseInt(amount, 10).toFixed(decimals));
-  const j = (i.length) > 3 ? i.length % 3 : 0;
-  const p = i.substr(j).replace(/(\d{3})(?=\d)/g, `$1${thousandSeparator}`);
-  return `${s}${(j ? i.substr(0, j) + thousandSeparator : '')}${p}${(decimals ? decimalMark + Math.abs(amount - i).toFixed(decimals).slice(2) : '')}`;
+  const ntzAmount = Math.abs(Number(babzAmount / NTZ_DECIMALS) || 0).toFixed(decimals);
+  const intAmount = String(parseInt(ntzAmount, 10));
+  const j = (intAmount.length) > 3 ? intAmount.length % 3 : 0;
+  const p = intAmount.substr(j).replace(/(\d{3})(?=\d)/g, `$1${thousandSeparator}`);
+  return `${s}${(j ? intAmount.substr(0, j) + thousandSeparator : '')}${p}${(decimals ? decimalMark + Math.abs(ntzAmount - intAmount).toFixed(decimals).slice(2) : '')}`;
 }
