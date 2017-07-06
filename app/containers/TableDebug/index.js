57041575d90059b42128bd4b546cb397642d0e1e
Alexander Lunyov <isuntc@gmail.com>
2017-07-06 23:26:45 +0800

    Format debug data

--- app/containers/TableDebug/index.js
+++ app/containers/TableDebug/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import * as PropTypes from 'prop-types';
 import styled from 'styled-components';
+import EWT from 'ethereum-web-token';
+import { formatNtz } from '../../utils/amountFormater';
 
 const Wrapper = styled.div`
   position: fixed;
   z-index: 1000;
-  bottom: 0;
-  right: 0;
+  top: 0;
+  left: 0;
   
+  overflow: auto;
   width: 80%;
+  max-height: 100vh;
   padding: 10px;
 
   opacity: 0.4;
@@ -21,7 +25,31 @@ const Wrapper = styled.div`
   }
 `;
 
+const Table = styled.table`
+  th, td {
+    text-align: left;
+  }
+
+  th {
+    padding: 0 10px;
+  }
+
+  td {
+    padding: 5px 10px;
+  }
+
+  tbody tr:nth-child(odd) {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
+
+  tbody td:nth-child(2n + 1):not(:last-child),
+  tbody th {
+    border-right: 1px solid #ccc;
+  }
+`;
+
 window.enableTableDebug = () => null;
+window.disableTableDebug = () => null;
 
 export default class TableDebug extends React.Component {
 
@@ -30,7 +58,7 @@ export default class TableDebug extends React.Component {
 
     this.state = {
       data: null,
-      visible: false,
+      visible: !!JSON.parse(localStorage.getItem('table_debug_enabled') || 'false'),
       expanded: false,
     };
 
@@ -39,12 +67,19 @@ export default class TableDebug extends React.Component {
 
     window.enableTableDebug = () => {
       this.setState({ visible: true, expanded: true });
+      localStorage.setItem('table_debug_enabled', true);
       this.handleRefresh();
+    };
+
+    window.disableTableDebug = () => {
+      this.setState({ visible: false });
+      localStorage.setItem('table_debug_enabled', false);
     };
   }
 
   componentWillUnmount() {
     window.enableTableDebug = () => null;
+    window.disableTableDebug = () => null;
   }
 
   handleExpandedToggle() {
@@ -58,6 +93,111 @@ export default class TableDebug extends React.Component {
         data: response,
       });
     });
+  }
+
+  renderDbHands(hands) {
+    const dists = hands.map((hand) => parseDistribution(hand.distribution));
+
+    return (
+      <div>
+        <Table>
+          <thead>
+            <tr>
+              <th>Hand Id</th>
+              {hands[0].lineup.map((_, j) =>
+                <th key={j} colSpan={2}>Seat {j}</th>
+              )}
+            </tr>
+            <tr>
+              <td />
+              {hands[0].lineup.reduce((memo, seat, j) => memo.concat([
+                <th key={j * 2}>
+                  Dist
+                </th>,
+                <th key={(j * 2) + 1}>
+                  Last
+                </th>,
+              ]), [])}
+            </tr>
+          </thead>
+          <tbody>
+            {hands.map((hand, i) => (
+              <tr key={hand.handId}>
+                <th>{hand.handId}</th>
+                {hand.lineup.reduce((memo, seat, j) => memo.concat([
+                  <td key={j * 2}>
+                    {dists[i] && renderNtz(dists[i][seat.address])}
+                  </td>,
+                  <td key={(j * 2) + 1}>
+                    {renderNtz(parseLastReceiptAmount(seat.last))}
+                  </td>,
+                ]), [])}
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <th>Exit hand</th>
+              {hands[0].lineup.map((seat, j) =>
+                <td key={j} colSpan={2}>{seat.exitHand}</td>
+              )}
+            </tr>
+          </tfoot>
+        </Table>
+      </div>
+    );
+  }
+
+  renderData(data) {
+    if (!data) {
+      return null;
+    }
+
+    return (
+      <div>
+        {JSON.stringify(data.contract)}
+
+        {this.renderDbHands(data.db)}
+
+        <ul>
+          <li>lastHandNetted: {data.contract.lastHandNetted}</li>
+          <li>lastNettingRequestHandId: {data.contract.lastNettingRequestHandId}</li>
+          <li>lastNettingRequestTime: {new Date(data.contract.lastNettingRequestTime * 1000).toString()}</li>
+        </ul>
+        <Table>
+          <thead>
+            <tr>
+              <th>Hand Id</th>
+              {data.db[0].lineup.map((_, i) =>
+                <th key={i} colSpan={2}>Seat {i}</th>)}
+            </tr>
+            <tr>
+              <th></th>
+              {data.db[0].lineup.reduce((memo, _, i) => memo.concat([
+                <th key={i * 2}>In</th>,
+                <th key={(i * 2) + 1}>Out</th>,
+              ]), [])}
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(data.contract.hands).map((handId) => (
+              <tr key={handId}>
+                <td>{handId}</td>
+                {data.db[0].lineup.reduce((memo, _, i) => memo.concat([
+                  <td key={i * 2}>
+                    {data.contract.hands[handId].ins[i]}
+                  </td>,
+                  <td key={(i * 2) + 1}>
+                    {data.contract.hands[handId].outs[i] &&
+                      data.contract.hands[handId].outs[i].out}
+                  </td>,
+                ]), [])}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
   }
 
   render() {
@@ -78,7 +218,8 @@ export default class TableDebug extends React.Component {
               refresh
             </button>
             <hr />
-            {JSON.stringify(data)}
+
+            {this.renderData(data)}
           </div>
         }
       </Wrapper>
@@ -91,3 +232,40 @@ TableDebug.propTypes = {
   tableService: PropTypes.object.isRequired,
   handId: PropTypes.number.isRequired,
 };
+
+function parseDistribution(distribution) {
+  if (!distribution) {
+    return {};
+  }
+
+  const { values } = EWT.parse(distribution);
+  const lineup = values[2];
+
+  return lineup.reduce((memo, seat) => {
+    const signerAddr = `0x${seat.slice(0, 40)}`;
+    const amount = parseInt(seat.slice(40), 16);
+
+    return {
+      ...memo,
+      [signerAddr]: amount,
+    };
+  }, {});
+}
+
+function parseLastReceiptAmount(receipt) {
+  if (!receipt) {
+    return null;
+  }
+
+  const { values } = EWT.parse(receipt);
+
+  return values[1];
+}
+
+function renderNtz(amount) {
+  if (amount) {
+    return `${formatNtz(amount)} NTZ`;
+  }
+
+  return (amount === null || amount === undefined) ? '-' : amount;
+}
