d60428981877175370eaa5c5e059c35a9673e957
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-07-21 17:45:57 +0900

    Separate receive section

--- app/components/Dashboard/Overview.js
+++ app/components/Dashboard/Overview.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'; import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import QRCode from 'qrcode.react';
 
 import {
   formatEth,
@@ -10,103 +8,27 @@ import {
 } from '../../utils/amountFormatter';
 
 import messages from '../../containers/Dashboard/messages';
-import UpgradeDialog from '../../containers/UpgradeDialog';
-import AccountProgress from '../../containers/Dashboard/AccountProgress';
-
-import Alert from '../Alert';
-import Button from '../Button';
 import WithLoading from '../WithLoading';
 import H2 from '../H2';
 import List from '../List';
 
+import SectionReceive from './SectionReceive';
 import {
-  Address,
   BalanceSection,
   BalanceWrapper,
   Pane,
   Section,
-  ReceiveWrapper,
-  ReceiveSection,
 } from './styles';
 
-const Overview = ({
-  ETH_FISH_LIMIT,
-  account,
+const Overview = (props, {
   babzBalance,
-  ethBalance,
-  nutzBalance,
-  floor,
   listTxns,
-  modalDismiss,
-  modalAdd,
   pwrBalance,
   weiBalance,
-  qrUrl,
 }) => (
   <Pane name="dashboard-overview">
     <Section name="wallet-receive">
-      <ReceiveSection>
-        <ReceiveWrapper
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            margin: '0 10px 0 0',
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-          }}
-        >
-          <WithLoading
-            isLoading={!account.proxy || account.proxy === '0x'}
-            loadingSize="40px"
-            styles={{ layout: { transform: 'translateY(-50%)', left: 0 } }}
-          >
-            <QRCode
-              value={qrUrl}
-              size={100}
-            />
-          </WithLoading>
-          <WithLoading
-            isLoading={!account.proxy || account.proxy === '0x'}
-            loadingSize="40px"
-            styles={{ layout: { transform: 'translateY(-50%)', left: 0 } }}
-          >
-            <Alert style={{ width: 220, marginBottom: 0 }} theme="success">
-              <Address>{account.proxy}</Address>
-            </Alert>
-          </WithLoading>
-        </ReceiveWrapper>
-
-        <ReceiveWrapper>
-          <Alert theme="danger">
-            <FormattedMessage {...messages.ethAlert} />
-          </Alert>
-
-          <Alert theme="warning">
-            Warning: account limit {ETH_FISH_LIMIT.toString()} ETH<br />
-            <Button
-              size="link"
-              onClick={() => modalAdd(
-                <UpgradeDialog
-                  proxyContract={account.proxy}
-                  onSuccessButtonClick={modalDismiss}
-                />
-              )}
-            >
-              Upgrade to shark account
-            </Button> to deposit more
-          </Alert>
-
-          {account.isLocked &&
-            ethBalance && nutzBalance && floor &&
-              <AccountProgress
-                ethBalance={ethBalance}
-                nutzBalance={nutzBalance}
-                floor={floor}
-                ethLimit={ETH_FISH_LIMIT}
-              />
-          }
-        </ReceiveWrapper>
-      </ReceiveSection>
+      <SectionReceive {...props} />
     </Section>
 
     <Section>
@@ -182,17 +104,9 @@ const Overview = ({
   </Pane>
 );
 Overview.propTypes = {
-  ETH_FISH_LIMIT: PropTypes.object,
-  account: PropTypes.object,
   babzBalance: PropTypes.object,
-  ethBalance: PropTypes.object,
-  nutzBalance: PropTypes.object,
-  floor: PropTypes.object,
   listTxns: PropTypes.array,
-  modalAdd: PropTypes.func,
-  modalDismiss: PropTypes.func,
   pwrBalance: PropTypes.object,
-  qrUrl: PropTypes.string,
   weiBalance: PropTypes.object,
 };
 
---
+++ app/components/Dashboard/SectionReceive.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import QRCode from 'qrcode.react';
+import { FormattedMessage } from 'react-intl';
+import messages from '../../containers/Dashboard/messages';
+
+import UpgradeDialog from '../../containers/UpgradeDialog';
+import AccountProgress from '../../containers/Dashboard/AccountProgress';
+import WithLoading from '../WithLoading';
+
+import Alert from '../Alert';
+import Button from '../Button';
+
+import {
+  Address,
+  ReceiveWrapper,
+  ReceiveSection,
+} from './styles';
+
+const SectionReceive = ({
+  ETH_FISH_LIMIT,
+  account,
+  ethBalance,
+  nutzBalance,
+  floor,
+  modalDismiss,
+  modalAdd,
+  qrUrl,
+}) => (
+  <ReceiveSection>
+    <ReceiveWrapper
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        margin: '0 10px 0 0',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+      }}
+    >
+      <WithLoading
+        isLoading={!account.proxy || account.proxy === '0x'}
+        loadingSize="40px"
+        styles={{ layout: { transform: 'translateY(-50%)', left: 0 } }}
+      >
+        <QRCode
+          value={qrUrl}
+          size={100}
+        />
+      </WithLoading>
+      <WithLoading
+        isLoading={!account.proxy || account.proxy === '0x'}
+        loadingSize="40px"
+        styles={{ layout: { transform: 'translateY(-50%)', left: 0 } }}
+      >
+        <Alert style={{ width: 220, marginBottom: 0 }} theme="success">
+          <Address>{account.proxy}</Address>
+        </Alert>
+      </WithLoading>
+    </ReceiveWrapper>
+
+    <ReceiveWrapper>
+      <Alert theme="danger">
+        <FormattedMessage {...messages.ethAlert} />
+      </Alert>
+
+      {account.isLocked &&
+        ethBalance && nutzBalance && floor &&
+        <Alert theme="warning">
+          Warning: account limit {ETH_FISH_LIMIT.toString()} ETH<br />
+          <Button
+            size="link"
+            onClick={() => modalAdd(
+              <UpgradeDialog
+                proxyContract={account.proxy}
+                onSuccessButtonClick={modalDismiss}
+              />
+            )}
+          >
+            Upgrade to shark account
+          </Button> to deposit more
+
+          <AccountProgress
+            ethBalance={ethBalance}
+            nutzBalance={nutzBalance}
+            floor={floor}
+            ethLimit={ETH_FISH_LIMIT}
+          />
+        </Alert>
+      }
+    </ReceiveWrapper>
+  </ReceiveSection>
+);
+SectionReceive.propTypes = {
+  ETH_FISH_LIMIT: PropTypes.object,
+  account: PropTypes.object,
+  ethBalance: PropTypes.object,
+  nutzBalance: PropTypes.object,
+  floor: PropTypes.object,
+  modalAdd: PropTypes.func,
+  modalDismiss: PropTypes.func,
+  qrUrl: PropTypes.string,
+};
+export default SectionReceive;
