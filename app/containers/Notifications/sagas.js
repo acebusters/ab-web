15ea7ec6db8d307d8bd90a2895d4595bcdadc04e
Alex Lunyov <isuntc@gmail.com>
2017-07-24 11:47:21 +0800

    Table join notification scenario

--- app/containers/Notifications/sagas.js
+++ app/containers/Notifications/sagas.js
@@ -1,4 +1,4 @@
-import { put, takeEvery, select, call } from 'redux-saga/effects';
+import { put, takeEvery, take, select, call } from 'redux-saga/effects';
 import { delay } from 'redux-saga';
 import uuid from 'uuid/v4';
 
@@ -10,7 +10,16 @@ import {
   notifyRemoving,
 } from './actions';
 
-import { SET_AUTH, ACCOUNT_LOADED, INJECT_ACCOUNT_UPDATE } from '../AccountProvider/actions';
+import {
+  SET_AUTH,
+  ACCOUNT_LOADED,
+  INJECT_ACCOUNT_UPDATE,
+  CONTRACT_TX_SEND,
+  CONTRACT_TX_SUCCESS,
+  CONTRACT_TX_FAIL,
+} from '../AccountProvider/actions';
+
+import { getWeb3 } from '../AccountProvider/utils';
 
 import {
   TEMP,
@@ -19,6 +28,8 @@ import {
   temp,
   persist,
 } from './constants';
+
+import { waitForTx } from '../../utils/waitForTx';
 
 function* createTempNotification(note) {
   yield put(notifyAdd(note));
@@ -75,12 +86,53 @@ function* injectedWeb3NotificationDismiss({ payload: injected }) {
   }
 }
 
+function* tableJoinNotification(sendAction) {
+  const state = yield select();
+  const table = yield call([state, state.get], 'table');
+  const isLocked = yield call([state, state.getIn], ['account', 'isLocked']);
+
+  console.log('checking if address is tableAddr', table.toJS(), sendAction.payload.args[0], sendAction.payload);
+
+  if (isLocked) { // do not need to show notification for shark account here
+    console.log('show pending table notification');
+  }
+
+  if (table.has(sendAction.payload.args[0])) {
+    while (true) { // eslint-disable-line no-constant-condition
+      const finalAction = yield take([CONTRACT_TX_FAIL, CONTRACT_TX_SUCCESS]);
+      try {
+        if (sendAction.payload.args[0] === finalAction.payload.args[0]) {
+          if (finalAction === CONTRACT_TX_SUCCESS) {
+            if (!isLocked) { // show notification for sharks (after submitting tx in metamask)
+              console.log('show pending table notification');
+            }
+
+            const web3 = yield call(getWeb3);
+            console.log('waitForTx', finalAction.payload.txHash);
+            yield call(waitForTx, web3, finalAction.payload.txHash);
+            console.log('show success table notification');
+          } else {
+            console.warn('fail');
+            throw finalAction;
+          }
+        }
+      } catch (e) {
+        console.log(e);
+        console.log('show error table notification');
+      } finally {
+        console.log('remove table notification');
+      }
+    }
+  }
+}
+
 export function* notificationsSaga() {
   yield takeEvery(SET_AUTH, authNotification);
   yield takeEvery(ACCOUNT_LOADED, injectedWeb3Notification);
   yield takeEvery(INJECT_ACCOUNT_UPDATE, injectedWeb3NotificationDismiss);
   yield takeEvery(NOTIFY_CREATE, selectNotification);
   yield takeEvery(NOTIFY_REMOVE, removeNotification);
+  yield takeEvery(CONTRACT_TX_SEND, tableJoinNotification);
 }
 
 export default [
