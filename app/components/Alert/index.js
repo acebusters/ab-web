ceb02e2219818e544d3fe2115fc714806ab8c5c9
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-09-19 03:37:32 +0700

    add info style to Alert boxes

--- app/components/Alert/index.js
+++ app/components/Alert/index.js
@@ -1,19 +1,23 @@
 import { PropTypes } from 'react';
 import styled from 'styled-components';
+import { gray } from '../../variables';
 
 const borderColors = {
+  info: '#eee',
   danger: '#dcb1b1',
   warning: '#ccbb99',
   success: '#d4e7c4',
 };
 
 const bgColors = {
+  info: '#f4f7f9',
   danger: '#ffcaca',
   warning: '#ffee99',
   success: '#e7efe4',
 };
 
 const textColors = {
+  info: gray,
   danger: '#634a49',
   warning: '#634a33',
   success: '#3a6536',
@@ -26,12 +30,17 @@ const Alert = styled.div`
   border-radius: 4px;
   color: ${(props) => textColors[props.theme]};
   background-color: ${(props) => bgColors[props.theme]};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 40px;
 `;
 
 export default Alert;
 
 Alert.propTypes = {
-  theme: PropTypes.oneOf(['success', 'warning', 'danger']),
+  theme: PropTypes.oneOf(['info', 'success', 'warning', 'danger']),
 };
 
 Alert.defaultProps = {
