90b37f1257dbaa8f297196cb27de4cbe8c023c1b
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-10-02 20:37:59 +0700

    Make component/Lobby a stateless component

--- app/components/Lobby/index.js
+++ app/components/Lobby/index.js
@@ -8,124 +8,71 @@ import { TableStriped } from 'components/List';
 import LobbyItem from 'containers/LobbyItem';
 import LobbyMessage from 'containers/LobbyMessage';
 
-import { fetchTableState, fetchTables } from '../../services/tableService';
-import { ABI_TABLE } from '../../app.config';
-
 import { Section } from './styles';
 
-async function getTableData(table, action) {
-  const [lineup, sb] = await Promise.all([
-    table.getLineup.callPromise(),
-    table.smallBlind.callPromise(),
-  ]);
+const Lobby = (props) => {
+  const { lobby, refreshing, handleRefresh } = props;
 
-  action(table.address, lineup, sb);
-}
+  return (
+    <Container>
+      <LobbyMessage
+        bookmark="lobby-msg"
+      />
 
-const getTableHand = (tableAddr, action) => fetchTableState(tableAddr).then((rsp) => action(tableAddr, rsp));
+      <TableStriped style={{ marginTop: 20 }}>
+        <thead>
+          <tr>
+            <th key="number">#</th>
+            <th key="blind">Blinds</th>
+            <th key="play">Players </th>
+            <th key="hand">Hand</th>
+            <th key="actn" />
+          </tr>
+        </thead>
+        {lobby && lobby.length > 0 && (
+          <tbody>
+            {lobby.map((tableAddr, i) =>
+              <LobbyItem key={i} tableAddr={tableAddr} />
+            )}
+          </tbody>
+        )}
+      </TableStriped>
 
-class Lobby extends React.PureComponent {
+      <WithLoading
+        isLoading={lobby.length === 0}
+      />
 
-  constructor(props) {
-    super(props);
-    this.handleRefresh = this.handleRefresh.bind(this);
-    this.web3 = props.web3Redux.web3;
-    this.state = {
-      refreshing: false,
-    };
-  }
-
-  componentDidMount() {
-    this.handleRefresh();
-  }
-
-  async handleRefresh() {
-    this.setState({ refreshing: true });
-    try {
-      const tables = await fetchTables();
-      if (tables) {
-        await Promise.all(tables.map((tableAddr) => {
-          const contract = this.web3.eth.contract(ABI_TABLE).at(tableAddr);
-          this.props.tableReceived(tableAddr);
-
-          return Promise.all([
-            getTableData(contract, this.props.lineupReceived),
-            getTableHand(tableAddr, this.props.updateReceived),
-          ]);
-        }));
-      }
-    } finally {
-      this.setState({ refreshing: false });
-    }
-  }
-
-  render() {
-    const { refreshing } = this.state;
-    const { lobby } = this.props;
-
-    return (
-      <Container>
-        <LobbyMessage
-          bookmark="lobby-msg"
-        />
-
-        <TableStriped style={{ marginTop: 20 }}>
-          <thead>
-            <tr>
-              <th key="number">#</th>
-              <th key="blind">Blinds</th>
-              <th key="play">Players </th>
-              <th key="hand">Hand</th>
-              <th key="actn" />
-            </tr>
-          </thead>
-          {lobby && lobby.length > 0 && (
-            <tbody>
-              {lobby.map((tableAddr, i) =>
-                <LobbyItem key={i} tableAddr={tableAddr} />
-              )}
-            </tbody>
-          )}
-        </TableStriped>
-
+      <Button
+        style={{ width: '100%' }}
+        onClick={handleRefresh}
+        size="medium"
+        disabled={refreshing}
+      >
         <WithLoading
-          isLoading={lobby.length === 0}
-        />
-
-        <Button
-          style={{ width: '100%' }}
-          onClick={this.handleRefresh}
-          size="medium"
-          disabled={refreshing}
+          isLoading={refreshing}
+          loadingSize="14px"
+          type="inline"
+          styles={{
+            inner: { marginLeft: 5 },
+          }}
         >
-          <WithLoading
-            isLoading={refreshing}
-            loadingSize="14px"
-            type="inline"
-            styles={{
-              inner: { marginLeft: 5 },
-            }}
-          >
-            Refresh
-          </WithLoading>
-        </Button>
+          Refresh
+        </WithLoading>
+      </Button>
 
 
-        <Section>
-          <DiscordWidget />
-        </Section>
+      <Section>
+        <DiscordWidget />
+      </Section>
 
-      </Container>
-    );
-  }
-}
+    </Container>
+  );
+};
 
 Lobby.propTypes = {
   lobby: PropTypes.array,
-  web3Redux: PropTypes.any,
-  tableReceived: PropTypes.func,
-  updateReceived: PropTypes.func,
-  lineupReceived: PropTypes.func,
+  refreshing: PropTypes.bool,
+  handleRefresh: PropTypes.func,
 };
 
 export default Lobby;
--- app/containers/Lobby/index.js
+++ app/containers/Lobby/index.js
@@ -1,8 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import Lobby from 'components/Lobby';
 import { createStructuredSelector } from 'reselect';
 import { tableReceived, lineupReceived, updateReceived } from '../Table/actions';
 import { makeSelectLobby } from './selectors';
 import web3Connect from '../AccountProvider/web3Connect';
+
+import { fetchTableState, fetchTables } from '../../services/tableService';
+import { ABI_TABLE } from '../../app.config';
+
+async function getTableData(table, action) {
+  const [lineup, sb] = await Promise.all([
+    table.getLineup.callPromise(),
+    table.smallBlind.callPromise(),
+  ]);
+
+  action(table.address, lineup, sb);
+}
+
+const getTableHand = (tableAddr, action) => fetchTableState(tableAddr).then((rsp) => action(tableAddr, rsp));
+
+class LobbyContainer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleRefresh = this.handleRefresh.bind(this);
+    this.web3 = props.web3Redux.web3;
+    this.state = {
+      refreshing: false,
+    };
+  }
+
+  componentDidMount() {
+    this.handleRefresh();
+  }
+
+  async handleRefresh() {
+    this.setState({ refreshing: true });
+    try {
+      const tables = await fetchTables();
+      if (tables) {
+        await Promise.all(tables.map((tableAddr) => {
+          const contract = this.web3.eth.contract(ABI_TABLE).at(tableAddr);
+          this.props.tableReceived(tableAddr);
+
+          return Promise.all([
+            getTableData(contract, this.props.lineupReceived),
+            getTableHand(tableAddr, this.props.updateReceived),
+          ]);
+        }));
+      }
+    } finally {
+      this.setState({ refreshing: false });
+    }
+  }
+
+  render() {
+    return (
+      <Lobby
+        refreshing={this.state.refreshing}
+        handleRefresh={this.handleRefresh}
+        {...this.props}
+      />
+    );
+  }
+}
+LobbyContainer.propTypes = {
+  web3Redux: PropTypes.any,
+  tableReceived: PropTypes.func,
+  updateReceived: PropTypes.func,
+  lineupReceived: PropTypes.func,
+};
 
 export function mapDispatchToProps() {
   return {
@@ -16,4 +83,4 @@ const mapStateToProps = createStructuredSelector({
   lobby: makeSelectLobby(),
 });
 
-export default web3Connect(mapStateToProps, mapDispatchToProps)(Lobby);
+export default web3Connect(mapStateToProps, mapDispatchToProps)(LobbyContainer);
