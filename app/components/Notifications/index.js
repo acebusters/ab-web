3d5a4e7737ee56e91bccb32f981d9a685f66df34
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-10-21 04:23:15 +0700

    refactor for cleaner code

--- app/components/Notifications/index.js
+++ app/components/Notifications/index.js
@@ -2,19 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Notification from './Notification';
 
-const Notifications = (props) => {
-  const { notifications } = props;
-
-  return (
-    <div>
-      {notifications.length !== 0 &&
-        notifications.map(
-          (item, i) => <Notification key={i} {...item} {...props} />,
-        )
-      }
-    </div>
-  );
-};
+const Notifications = (props) => (
+  <div>
+    {props.notifications.length !== 0 &&
+      props.notifications.map(
+        (item, i) => <Notification key={i} {...item} {...props} />,
+      )
+    }
+  </div>
+);
 Notifications.propTypes = {
   notifications: PropTypes.array,
 };
