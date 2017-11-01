1e5800652dfecfc1f3504218be67d23f54edefc6
Jonathan Zobro <jonathan.zobro@gmail.com>
2017-11-01 20:55:02 +0700

    add basic board card animations

--- app/components/Board/styles.js
+++ app/components/Board/styles.js
@@ -1,4 +1,15 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const cardAnim = keyframes`
+  0% {
+    opacity: 0.3;
+    transform: translateY(-500%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0%);
+  }
+`;
 
 export const BoardWrapper = styled.div`
   margin: 0;
@@ -13,4 +24,5 @@ export const BoardWrapper = styled.div`
 export const CardWrapper = styled.div`
   float: left;
   margin-left: 0.5em;
+  animation: ${cardAnim} 0.6s cubic-bezier(0.57, 0.2, 0.75, 1.1);
 `;
