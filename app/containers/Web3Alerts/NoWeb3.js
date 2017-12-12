8c10e81885f619297f8da01d5298f67a5217a46a
Alex Lunyov <isuntc@gmail.com>
2017-12-12 17:35:35 +0800

    Add browser links to metamask error. Explaination about account unlock

--- app/containers/Web3Alerts/NoWeb3.js
+++ app/containers/Web3Alerts/NoWeb3.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import A from '../../components/A';
 import { Wrapper } from './styles';
 
 const NoWeb3Alert = () => (
@@ -7,8 +8,8 @@ const NoWeb3Alert = () => (
     <p>
       You read from the Ethereum blockchain,
       but in order to interact with it from your browser,
-      download an Ethereum enabled browser like Mist,
-      Parity or install the Metamask Chrome Extension.
+      download an Ethereum enabled browser like <A href="https://github.com/ethereum/mist#installation" target="_blank">Mist</A>,
+      <A href="https://www.parity.io/" target="_blank">Parity</A> or install the <A href="https://metamask.io/" target="_blank">Metamask Chrome Extension</A>.
     </p>
   </Wrapper>
 );
