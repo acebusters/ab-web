ee13cffc7d8c2dc28c8b9e12d88839869ee0c941
Alex Lunyov <isuntc@gmail.com>
2018-01-22 16:38:57 +0800

    Generate wallet and store it in localStorage

--- app/containers/App/sagas/index.js
+++ app/containers/App/sagas/index.js
@@ -1,14 +1,44 @@
-import { fork, takeEvery } from 'redux-saga/effects';
+import { call, fork, takeEvery, put } from 'redux-saga/effects';
+import ethers from 'ethers';
 
-import { ACCOUNT_LOADED } from '../../AccountProvider/actions';
+import { ACCOUNT_LOADED, setAuthState } from '../../AccountProvider/actions';
+
+import * as storageService from '../../../services/localStorage';
 
 import gtmSaga from './gtmSagas';
 import unsupportedModalSaga from './unsupportedModalSaga';
 import balancesLoadingSaga from './balancesLoadingSaga';
 
+const totalBits = 768;
+const bitsToBytes = (bits) => bits / 8;
+
+function getWallet() {
+  const crypto = window.crypto || window.msCrypto;
+  if (storageService.getItem('wallet')) {
+    const { mnemonic } = JSON.parse(storageService.getItem('wallet'));
+    return ethers.Wallet.fromMnemonic(mnemonic);
+  }
+
+  const wallet = ethers.Wallet.createRandom({
+    extraEntropy: Array.from(crypto.getRandomValues(new Uint8Array(bitsToBytes(totalBits)))),
+  });
+
+  storageService.setItem('wallet', JSON.stringify(wallet));
+
+  return wallet;
+}
+
+function* walletSaga() {
+  const wallet = yield call(getWallet);
+  yield put(setAuthState({
+    privKey: wallet.privateKey,
+    loggedIn: true,
+  }));
+}
 
 export function* appSaga(dispatch) {
   yield fork(gtmSaga);
+  yield fork(walletSaga);
   yield fork(balancesLoadingSaga, dispatch);
   yield takeEvery(ACCOUNT_LOADED, unsupportedModalSaga);
 }
