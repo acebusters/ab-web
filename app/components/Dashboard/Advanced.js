eb9325548135d9b817791b9160885110f570234d
Jonathan Zobro <jonathan.zobro@gmail.com>
2018-02-05 17:50:57 +0900

    init 'advanced' route and tab to dashboard

---
+++ app/components/Dashboard/Advanced.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Advanced extends React.Component {
+  // static propTypes = {}
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div>
+        <h3>Wallet Recovery</h3>
+        <p>Explanation of these functions</p>
+        <i>Testnet only warning!</i>
+        <ul>
+          <li>Import Wallet</li>
+          <li>Export Wallet</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Advanced;
---
+++ app/containers/Dashboard/Advanced.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+import AdvancedComponent from 'components/Dashboard/Advanced';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Advanced extends React.Component {
+  // static propTypes = {};
+  render() {
+    return <AdvancedComponent />;
+  }
+}
+
+export default Advanced;
