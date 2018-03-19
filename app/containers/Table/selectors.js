70cbf77430a1ba15a6e196cfc09725f70305f0ee
Alex Lunyov <isuntc@gmail.com>
2018-03-19 23:41:34 +0800

    Show hand blinds on lobby page

--- app/containers/Table/selectors.js
+++ app/containers/Table/selectors.js
@@ -15,13 +15,21 @@ const rc = new ReceiptCache();
 const pokerHelper = new PokerHelper(rc);
 
 // direct selectors to state
-export const tableStateSelector = (state, props) => (state && props) ? state.getIn(['table', props.params.tableAddr]) : null;
+export const tableStateSelector = (state, props) => {
+  if (state && props) {
+    const tableAddr = props.tableAddr || props.params.tableAddr;
+    return state.getIn(['table', tableAddr]);
+  }
+
+  return null;
+};
 
 export const handSelector = (state, props) => {
   if (state && props) {
+    const tableAddr = props.tableAddr || props.params.tableAddr;
     const handId = props.latestHand || makeLatestHandSelector()(state, props);
     if (handId !== null) {
-      return state.getIn(['table', props.params.tableAddr, String(handId)]);
+      return state.getIn(['table', tableAddr, String(handId)]);
     }
   }
 
